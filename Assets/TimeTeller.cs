a830fa261c47030fc450b9abd6c2c3ac55560fb8
Harish Venkataraman <harishvenkat238@gmail.com>
2020-09-11 06:34:59 -0500

    Add Clock widget

---
+++ Assets/TimeTeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeTeller : MonoBehaviour
+{
+    public GameObject timeTextObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ChangeTime", 0f, 10f);
+    }
+
+    // Update is called once per frame
+    void ChangeTime()
+    {
+        timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("h:mm tt");
+    }
+}
