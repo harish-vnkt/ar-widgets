47ab347f093383c5a11161623099707efda16af4
Harish Venkataraman <harishvenkat238@gmail.com>
2020-09-17 00:35:34 -0500

    Create WeatherData class to deserialize JSON response

--- Assets/Scripts/WeatherData.cs
+++ Assets/Scripts/WeatherData.cs
@@ -1,18 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class WeatherData : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[System.Serializable]
+public class Coord {
+    public float lon;
+    public float lat;
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+public class Weather {
+    public int id;
+    public string main;
+    public string description;
+    public string icon;
+}
+
+[System.Serializable]
+public class Main {
+    public float temp;
+    public float feels_like;
+    public float temp_min;
+    public float temp_max;
+    public float pressure;
+    public float humidity;
+}
+
+[System.Serializable]
+public class Wind {
+    public float speed;
+    public float deg;
+    public float gust;
+}
+
+[System.Serializable]
+public class Clouds {
+    public int all;
+}
+
+[System.Serializable]
+public class Sys {
+    public int type;
+    public int id;
+    public string country;
+    public int sunrise;
+    public int sunset;
+}
+
+[System.Serializable]
+public class WeatherData
+{   
+    public Coord coord;
+    public Weather[] weathers;
+    public Main main;
+    public Wind wind;
+    public Clouds clouds;
+    public Sys sys;
+
+    public int visibility;
+    public int dt;
+    public int timezone;
+    public int id;
+    public string name;
+    public int cod;
+
 }
